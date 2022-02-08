aa4aed18ece23b827dd163d6a3e25e58abbc69f1
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-02-07 21:28:19 -0600

    main body headers

---
+++ src/components/main.js
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <div>
+      <h1>Josh Stotts</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum
+        accusantium nesciunt aperiam? Iste totam sapiente, expedita voluptates
+        nostrum culpa! Adipisci quod magni, perferendis voluptatum omnis
+        consequatur ullam eum ad quidem.
+      </p>
+      <h2>Skills</h2>
+      <h2>Projects</h2>
+      <h2>About Me</h2>
+      <h3>Contact Me!</h3>
+    </div>
+  );
+}
+
+export default Main;
