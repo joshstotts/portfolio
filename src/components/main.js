2f30c3cb0f9f5e39b6b12277f2e748d54acd4d84
Josh Stotts <joshstotts@Joshs-MacBook-Pro.local>
2022-02-08 22:46:15 -0600

    changed fonts

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import '../main.css';
+import { Link } from 'react-router-dom';
 
 function Main() {
   return (
@@ -14,6 +15,7 @@ function Main() {
           <img src="https://www.logolynx.com/images/logolynx/97/97e9aeba921974016df5da3abe701319.png" alt="HTML 5" />
           <img src="https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/042015/css3.png?itok=OlYIVwA0" alt="CSS 3" />
           <img src="https://infinapps.com/wp-content/uploads/2018/10/mongodb-logo.png" alt="MongoDB logo" />
+          <img src="https://quantlabs.net/wp-content/uploads/2019/12/python-main-qimg-28cadbd02699c25a88e5c78d73c7babc.png" alt="python logo" />
           <img src="https://openjsf.org/wp-content/uploads/sites/84/2019/10/jquery-logo-vertical_large_square.png" alt="jQuery logo" />
 
         </div>
@@ -21,22 +23,21 @@ function Main() {
       <div id='projects'>
         <h3 id='p1' >Mario Kart</h3>
         <div id='mario' ><img id='mariopic' src="https://i.imgur.com/b3SwPCe.png" alt="Mario Kart homepage" /></div>
-        <div id='travel' ><img id='travelpic' src="https://i.imgur.com/cEZZSt5.png" alt="travel homepage" /></div>
+        <Link to='https://travel-app-sp.netlify.app/' id='travel' ><img id='travelpic' src="https://i.imgur.com/cEZZSt5.png" alt="travel homepage" /></Link>
         <h3 id='p2' >World Traveler</h3>
-        <h3 id='p3' >Pokedex</h3>
+        <h3 id='p3' >Pokédex</h3>
         <div id='pokedex'><img id='pokedexpic' src="https://i.imgur.com/kkXkFBC.png" alt="pokedex homepage" /></div>
+        <div id='stocks' ><img id='stockspic' src="https://i.imgur.com/paoFVae.png" alt="stocks homepage" /></div>
+        <h3 id='p4' >Stock Watch</h3>
       </div>
-      {/* <h2>About Me</h2>
-      <p></p> */}
+  
       <h4>Contact Me!</h4>
 
       <div id='c1'>
-        <img id='emailicon' src="https://olc-wordpress-assets.s3.amazonaws.com/uploads/2020/05/new-email-icon.png" alt="email icon" />
         <p id='email' >joshstotts@outlook.com</p>
       </div>
 
       <div id='c2'>
-        <img id='phoneicon' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRslmAJE0i0prxQAj_pueZKmLCZhWy3frKr60a1nZUie5hRpgNGThuVGOJ2k_ularlGAI0&usqp=CAU" alt="phone icon" />
         <p id='phone' >515-657-1096</p>
       </div>
 
